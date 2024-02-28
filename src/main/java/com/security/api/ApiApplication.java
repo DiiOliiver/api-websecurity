45512c225d86dbfd6ea658caa888d662e817e451
Diego Feitosa de Oliveira <diego.feitosa.oliveira@hotmail.com>
2024-02-27 21:44:30 -0400

    feat: subindo projeto com websecurity no pom.xml

--- src/main/java/com/security/api/ApiApplication.java
+++ src/main/java/com/security/api/ApiApplication.java
@@ -1,21 +1,10 @@
 package com.security.api;
 
-import com.security.api.configuration.AuditableConfig;
-import com.security.api.model.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.domain.AuditorAware;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 public class ApiApplication {
-
-	@Bean
-	public AuditorAware<User> auditorAware() {
-		return new AuditableConfig();
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
